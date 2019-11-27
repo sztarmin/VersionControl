cc330d880959746ff9769825d032d4b4c858e593
Ármin Atrakocsi <aarminofficial@gmail.com>
2019-11-27 22:46:56 +0100

    Export functionality

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.lblLastName = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -67,11 +68,22 @@
             this.btnAdd.Text = "button1";
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.lblLastName);
             this.Controls.Add(this.txtLastName);
@@ -89,6 +101,7 @@
         private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Label lblLastName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             lblLastName.Text = Resource1.FullName;
             btnAdd.Text = Resource1.Add;
+            button1.Text = Resource1.wtf;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -34,6 +36,32 @@ namespace UserMaintenance
                 FullName = txtLastName.Text
             };
             users.Add(u);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.ShowDialog();
+            String fn = saveFileDialog1.FileName;
+            if (fn != "")
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter(fn);
+                    sw.WriteLine("id,name");
+                    //using for as it's faster than foreach
+                    for (int i = 0; i < users.Count; i++)
+                    {
+                        sw.WriteLine(users[i].ID + "," + users[i].FullName);
+                    }
+
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occured: " + ex.Message);
+                }
+            }
         }
     }
 }
